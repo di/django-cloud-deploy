4a7b96fc958bd3b3316c97e1b2ddcec9b2f59764
Xiaofei Wang <6218006+wangxf123456@users.noreply.github.com>
2018-12-06 15:41:49 -0800

    Add error handler to direct users to create Github issue.

--- django_cloud_deploy/crash_handling/__init__.py
+++ django_cloud_deploy/crash_handling/__init__.py
@@ -13,37 +13,96 @@
 # limitations under the License.
 """A module handles crash of the tool."""
 
+import os
+import platform
+import subprocess
+import sys
 import traceback
+import urllib.parse
 import webbrowser
+
+import jinja2
 
 from django_cloud_deploy.cli import io
 
 
+# TODO: Add an issue label for issues reported by users
 _REQUEST_URL_TEMPLATE = (
     ('https://github.com/GoogleCloudPlatform/django-cloud-deploy/issues/new?'
      'body={}'))
 
-_ISSUE_TEMPLATE = 'bar'
+
+with open(os.path.join(os.path.dirname(__file__), 'template',
+                       'issue_template.txt')) as f:
+    _ISSUE_TEMPLATE = f.read()
 
 
-def handle_crash(err: Exception, console: io.IO = io.ConsoleIO()):
+def handle_crash(err: Exception, command: str,
+                 console: io.IO = io.ConsoleIO()):
+    """The tool's crashing handler.
+
+    Args:
+        err: The exception get thrown.
+        command: The command causing the exception to get thrown.
+        console: Object to use for user I/O.
+    """
+
+    # TODO: Only handle crashes caused by our code, not user's code.
     console.tell(
         'django-cloud-deploy crashed with ({}): {}'.format(
             type(err).__name__, err))
 
     ans = console.ask(
-        'Would you like to file a bug against Django Cloud Deploy? If you '
-        'want, we will open a browser to direct you to the tool\'s Github '
-        'repo. The bug will include information about your environment but you '
-        'can remove any information that you don\'t want to share [y/N]: ')
+        'Would you like to file a bug against Django Cloud Deploy?\n'
+        'If you want, we will open a browser to direct you to the\n'
+        'tool\'s Github repo. The bug will include information about\n'
+        'your environment but you can remove any information that you\n'
+        'don\'t want to share [y/N]: ')
 
     while ans.lower() not in ['y', 'n']:
         ans = console.ask('Please answer "y" or "N": ')
 
     if ans.lower() == 'y':
-        _create_issue(_ISSUE_TEMPLATE)
+        _create_issue(command)
 
 
-def _create_issue(content: str):
-    url = _REQUEST_URL_TEMPLATE.format(content)
+def _create_issue(command: str):
+    """Open browser to create a issue on the package's Github repo.
+
+    Args:
+        command: What command we want to create issue for.
+    """
+    template_env = jinja2.Environment()
+    try:
+        gcloud_version = subprocess.check_output(
+            ['gcloud', 'info', '--format=value(basic.version)'],
+            universal_newlines=True).rstrip()
+    except subprocess.CalledProcessError:
+        gcloud_version = 'Not installed or not on PATH'
+
+    try:
+        docker_version = subprocess.check_output(
+            ['docker', '--version'], universal_newlines=True).rstrip()
+    except subprocess.CalledProcessError:
+        docker_version = 'Not installed or not on PATH'
+
+    try:
+        cloud_sql_proxy_version = subprocess.check_output(
+            ['cloud_sql_proxy', '--version'], universal_newlines=True).rstrip()
+    except subprocess.CalledProcessError:
+        cloud_sql_proxy_version = 'Not installed or not on PATH'
+
+    # TODO: Add django-cloud-deploy version
+    template = template_env.from_string(_ISSUE_TEMPLATE)
+    options = {
+        'command': command,
+        'gcloud_version': gcloud_version,
+        'docker_version': docker_version,
+        'cloud_sql_proxy_version': cloud_sql_proxy_version,
+        'python_version': sys.version.replace('\n', ' '),
+        'traceback': traceback.format_exc(),
+        'platform': platform.platform(),
+    }
+    content = template.render(options)
+    url = urllib.parse.quote(_REQUEST_URL_TEMPLATE.format(content), safe='/:?=')
     webbrowser.open(url)
